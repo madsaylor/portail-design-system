6d70498f17c1f9c07abf9cd3bff4204ad531b948
Ignat Shining <mail@igonato.com>
2018-12-05 18:02:19 +0300

    GH pages deploy script

---
+++ deploy-gh-pages.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# deploy
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:betao-dev/portail-design-system.git master:gh-pages
+
+# cd back
+cd -

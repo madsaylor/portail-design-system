da0c1065e5f0cf5413a60c312236828614ed178f
anton <anton.serdiukov.dev@gmail.com>
2019-06-04 21:35:50 +0300

    pagination wip

---
+++ src/components/Pagination.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="pagination-wrapper" v-if="value.length">
+    <div class="pagination-header">
+      <Card class="header-wrapper">
+        <div class="header"
+             v-for="(header, index) in headers"
+             :style="getFlex(index)">
+          {{header}}
+        </div>
+      </Card>
+    </div>
+    <div class="pagination-body-wrapper">
+      <div class="pagination-body" v-for="client in getClients">
+        <Card class="client-wrapper">
+        <span v-for="(value, key, index) in client"
+              :style="getFlex(index)">
+          {{value}}
+        </span>
+        </Card>
+      </div>
+    </div>
+    <div class="pagination-count">
+      <span class="count">{{this.startNumber}}-{{this.endNumber}} of {{count}}</span>
+      <Icon source="left-arrow" size="12px" padding="28px 24px 0px" @click="previous" :disabled="current === 1"/>
+      <Icon source="right-arrow" size="12px" padding="28px 0px 0px" @click="next"/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Icon from './Icon'
+  import Card from './Card'
+
+    export default {
+      name: 'Pagination',
+      components: {Icon, Card},
+      props: {
+        value: Array,
+        count: Number,
+        size: Number,
+        current: Number,
+        ratios: {
+          type: Array,
+          default: () => new Array(100).fill('1')
+        }
+      },
+      computed: {
+        startNumber() {
+          return (this.current - 1) * this.size + 1
+        },
+        endNumber() {
+          let temporaryCount = this.current * this.size
+          return temporaryCount > this.count ? this.count : temporaryCount
+        },
+        headers() {
+          if (this.value.length) {
+            return Object.keys(this.value[0])
+          }
+        },
+        getClients() {
+          return this.value.slice(this.startNumber - 1, this.endNumber)
+        }
+      },
+      methods: {
+        previous() {
+          this.$emit('previous')
+        },
+        next() {
+          this.$emit('next')
+        },
+        getFlex(index) {
+          return {
+            flex: this.ratios[index]
+          }
+        }
+      }
+    }
+</script>
+
+<style lang="less" scoped>
+  @import '../styles/vars';
+
+  .pagination-wrapper {
+    width: 960px;
+
+    .pagination-header {
+      .header-wrapper {
+        display: flex;
+
+        .header {
+          text-transform: capitalize;
+        }
+      }
+    }
+
+    .pagination-body-wrapper {
+      .pagination-body {
+        .client-wrapper {
+          display: flex;
+          margin-bottom: 16px;
+        }
+      }
+    }
+
+    .pagination-count {
+      display: flex;
+      justify-content: flex-end;
+
+      .count {
+          padding: 24px 0 0;
+      }
+
+      .icon {
+        &:disabled {
+          pointer-events: none;
+          svg {
+            fill: @color-gray-300;
+          }
+        }
+      }
+    }
+  }
+</style>
---
+++ src/demos/components/PaginationDemo.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="row-col">
+      <h3>Pagination</h3>
+      <p>Pagination component for display data like table with pagination</p>
+
+      <Description compnent-name="Pagination"></Description>
+
+      <Collapser :opened.sync="openUsage" label="Usage">
+        <pre v-highlightjs="usage"><code class="html"></code></pre>
+      </Collapser>
+
+      <Pagination v-model="clients"
+                  :count="count"
+                  :size="size"
+                  :current="current"
+                  :ratios="ratios"
+                  @previous="onPrevious"
+                  @next="onNext">
+      </Pagination>
+    </div>
+</template>
+
+<script>
+  import Pagination from '../../components/Pagination'
+  import Collapser from '../../components/Collapser'
+  import Description from '../../descriptions/Description'
+
+let usage = `
+
+`.slice(1)
+
+  export default {
+    name: 'PaginationDemo',
+    components: {Pagination, Collapser, Description},
+    data: () => ({
+      usage,
+      openUsage: true,
+      clients: [
+          {
+            name: 'Adam L1',
+            type: 'Person',
+            earned: '€ 3,123.34'
+          },
+          {
+            name: 'Adam L2',
+            type: 'Person',
+            earned: '€ 3,123.34'
+          },
+          {
+            name: 'Adam L3',
+            type: 'Person',
+            earned: '€ 3,123.34'
+          },
+          {
+            name: 'Adam L4',
+            type: 'Person',
+            earned: '€ 3,123.34'
+          },
+          {
+            name: 'Adam L5',
+            type: 'Person',
+            earned: '€ 3,123.34'
+          },
+          {
+            name: 'Adam L6',
+            type: 'Person',
+            earned: '€ 3,123.34'
+          },
+          {
+            name: 'Adam L7',
+            type: 'Person',
+            earned: '€ 3,123.34'
+          },
+          {
+            name: 'Adam L7',
+            type: 'Person',
+            earned: '€ 3,123.34'
+          }
+      ],
+      count: 8,
+      size: 3,
+      current: 1,
+      ratios: ['8', '1', '1']
+    }),
+    methods: {
+      onPrevious () {
+        this.current--
+      },
+      onNext () {
+        this.current++
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>

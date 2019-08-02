c7964096cd079b71817f39ae74ccaf203b08415a
Donghai <priceofjs@gmail.com>
2019-08-01 22:53:23 -0700

    Add Confirm dialog for temporarily

---
+++ src/components/ConfirmDialog.vue
@@ -0,0 +1,62 @@
+<template>
+  <Dialog
+    :opened.sync="opened"
+    :borderColor="borderColor"
+  >
+    <div
+      class="ds-confirm-wrapper"
+      :style="getStyle"
+    >
+      <div class="ds-confirm-header">
+        {{ title }}
+      </div>
+
+      <div class="ds-confirm-body">
+        <slot name="ds-confirm-content"></slot>
+      </div>
+
+      <div class="ds-confirm-footer">
+        <Button>Cancel</Button>
+        <Button>Accept</Button>
+      </div>
+    </div>
+  </Dialog>
+</template>
+
+<script>
+import Dialog from './Dialog'
+import Button from './Button'
+
+export default {
+  name: 'confirm-dialog',
+  components: {Dialog, Button},
+  props: {
+    opened: {
+      type: Boolean,
+      default: false
+    },
+    borderColor: {
+      type: String,
+      default: '#eeeeee'
+    },
+    width: {
+      type: String,
+      default: '544px'
+    },
+    title: String
+  },
+  computed: {
+    getStyle() {
+      const style = {}
+      style.width = this.width
+      return style
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.ds-confirm-wrapper {
+  padding: 32px 40px;
+}
+</style>
---
+++ src/demos/components/ConfirmDialogDemo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="row-col">
+    <h3>Confirm Dialog</h3>
+    <Description component-name="ConfirmDialog"></Description>
+    <Collapser :opened.sync="openUsage" label="Usage">
+      <pre v-highlightjs="usage">
+        <code class="html"></code>
+      </pre>
+    </Collapser>
+
+    <div class="button-group">
+      <Button class="button-modal" @click="opened = true">OPEN CONFIRM DIALOG</Button>
+    </div>
+
+    <ConfirmDialog
+      :opened.sync="opened"
+      :borderColor="borderColor"
+      title="Dialog box title"
+    >
+      <div slot="ds-confirm-content">
+        Donec facilisis tortor ut augue lacinia, at viverra est semper. Sed sapien metus, scelerisque nec pharetra id, tempor a tortor. Pellentesque non dignissim neque. 
+      </div>
+    </ConfirmDialog>
+  </div>
+</template>
+
+<script>
+import ConfirmDialog from '../../components/ConfirmDialog'
+import Button from '../../components/Button'
+import Description from '../../descriptions/Description'
+import Collapser from '../../components/Collapser'
+
+export default {
+  name: 'ConfirmDialogDemo',
+  components: { ConfirmDialog, Button, Description, Collapser },
+  data() {
+    return ({
+      opened: false,
+      borderColor: '#e6e7eb',
+      usage: 'test'
+    })
+  }
+}
+</script>
+
+<style lang="less">
+</style>

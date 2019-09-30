f81a65acfd06766ae9877d2a48a83bb903a909e8
Donghai <priceofjs@gmail.com>
2019-09-29 23:57:22 -0700

    Update validator in select component

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -132,7 +132,7 @@
         for (let i = 0; i < this.validators.length; i++) {
           data.push([
             this.validators[i].name,
-            this.validators[i].validator(this.inputSelectValue)
+            this.validators[i].validator(this.value)
           ])
         }
 

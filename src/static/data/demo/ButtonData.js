a5a6053270e279ce700aeb262516f2c2097e8ae1
Donghai <priceofjs@gmail.com>
2019-11-21 19:56:27 -0800

    Update button description and demo

--- src/static/data/demo/ButtonData.js
+++ src/static/data/demo/ButtonData.js
@@ -14,10 +14,14 @@ let usage = `
 <Button icon="menu"></Button>&nbsp;
 <Button link="https://github.com/highqualitycode" plain>Click here</Button>
 <Button link="https://github.com/highqualitycode" icon="account_circle">Github</Button>&nbsp;
+<<<<<<< e2fdfe3f181de4887f944f9c73c1842ca86f2ea6
 <Button link="https://github.com/highqualitycode" icon="insert_chart" small link-ico>Full Report</Button>&nbsp;
 <Button padding="15px 40px">Add</Button>
 <Button padding="13px 40px" alt-two>Cancel</Button>
 <Button icon="add_circle" icon-color="primary" color="primary" plain-two space-between="4px">Add New Item</Button>
+=======
+<Button color="red" link="https://github.com/highqualitycode" icon="insert_chart" small link-ico>Full Report</Button>&nbsp;
+>>>>>>> Update button description and demo
 <!-- OR -->
 <button class="ds-button ds-button-primary">
   Button Label
